8b61b72a23e08a2c398a0e209235b6cc0f1fadbe
gabriel streck <gabriel@apperto.co>
2023-02-01 16:43:19 -0300

    I add strapi to the project

---
+++ strapi/src/api/user-profile/controllers/user-profile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-profile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-profile.user-profile');
